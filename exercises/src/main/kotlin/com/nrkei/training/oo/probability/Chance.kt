e9de26d7ee72981a862a8ee8989b72fd8bd842fe
Fred George <fredgeorge@acm.org>
2026-01-26 16:20:53 +0400

    Fred: Chance with and(); rounding errors handled

--- exercises/src/main/kotlin/com/nrkei/training/oo/probability/Chance.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/probability/Chance.kt
@@ -10,15 +10,17 @@ package com.nrkei.training.oo.probability
 class Chance(likelihoodAsFraction: Number) {
     companion object {
         private const val CERTAIN_FRACTION = 1.0
+        private const val EPSILON = 1e-10
     }
     private val fraction = likelihoodAsFraction.toDouble()
 
     override fun equals(other: Any?) =
         this === other || other is Chance && this.equals(other)
 
-    private fun equals(other: Chance) = this.fraction == other.fraction
+    private fun equals(other: Chance) =
+        ((this.fraction - other.fraction) / EPSILON)< EPSILON
 
-    override fun hashCode() = fraction.hashCode()
+    override fun hashCode() = (fraction/EPSILON).toLong().hashCode()
 
     operator fun not() = Chance(CERTAIN_FRACTION - fraction)
 
