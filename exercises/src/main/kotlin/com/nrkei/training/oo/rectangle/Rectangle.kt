4dbb846cca42e3031b9bd63ed34bf3ae7fda6c64
Fred George <fredgeorge@acm.org>
2026-01-28 07:31:29 +0100

    Fred: Square as a kind of Rectangle

--- exercises/src/main/kotlin/com/nrkei/training/oo/rectangle/Rectangle.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/rectangle/Rectangle.kt
@@ -2,6 +2,9 @@ package com.nrkei.training.oo.rectangle
 
 // Understands a four-sided polygon with sides at right angles
 class Rectangle(length: Number, width: Number) {
+    companion object {
+        fun square(side: Number) = Rectangle(side, side)
+    }
     private val length = length.toDouble()
     private val width = width.toDouble()
     init {
@@ -12,5 +15,5 @@ class Rectangle(length: Number, width: Number) {
     fun area() = length * width
     val area get() = area()
     fun perimeter() = (length + width) * 2.0
-
+    val perimeter get() = perimeter()
 }
--- tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
+++ tests/src/test/kotlin/com/nrkei/training/oo/unit/RectangleTest.kt
@@ -12,15 +12,18 @@ internal class RectangleTest {
     fun area() {
         assertEquals(24.0, Rectangle(4.0, 6.0).area())
         assertEquals(24.0, Rectangle(4, 6).area)
+        assertEquals(25.0, Rectangle.square(5).area())
     }
 
     @Test fun perimeter() {
         assertEquals(20.0, Rectangle(4.0, 6.0).perimeter())
-        assertEquals(20.0, Rectangle(4, 6).perimeter())
+        assertEquals(20.0, Rectangle(4, 6).perimeter)
+        assertEquals(20.0, Rectangle.square(5.0).perimeter())
     }
 
     @Test fun `value dimension`() {
-        assertThrows<IllegalArgumentException> { Rectangle(0, 4)  }
-        assertThrows<IllegalArgumentException> { Rectangle(6, 0)  }
+        assertThrows<IllegalArgumentException> { Rectangle(0, 4) }
+        assertThrows<IllegalArgumentException> { Rectangle(6, 0) }
+        assertThrows<IllegalArgumentException> { Rectangle.square(0) }
     }
 }
